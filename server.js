ccbe85421263aa3368c6ba891585262be0ffcd57
nizarhalloun <nshalloun@hotmail.com>
2020-12-01 21:36:37 +0200

    init5

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'hello' }));
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
