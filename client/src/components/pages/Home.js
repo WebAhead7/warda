f24e474313d3c82727901cb4ae161d1872970d06
nizarhalloun <nshalloun@hotmail.com>
2020-12-03 14:04:19 +0200

    fix

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1 style={{ marginTop: '20px', textAlign: 'center' }}>Welcome ! </h1>
+      <h1 style={{ marginTop: '20px', textAlign: 'center' }}>
+        Welcome To Warda !{' '}
+      </h1>
+      <p className="flow-text" style={{ textAlign: 'center' }}>
+        You can find your match here.
+        <br />
+        Posting in completely anonymous
+      </p>
     </div>
   );
 };
